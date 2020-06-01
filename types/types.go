ed4d13c8bf59ac3204db7d29ab5312665a759093
nakadayoshiki <y.nakada.rkt@gmail.com>
2020-06-01 09:16:00 +0900

    types succes #1

---
+++ types/types.go
@@ -0,0 +1,47 @@
+package types
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+)
+
+// User 定義
+type User struct {
+	gorm.Model
+	Name     string `gorm:"not null"`
+	Email    string `gorm:"unique_index"`
+	Password string `gorm:"not null"`
+}
+
+const (
+	mysqlUsersUserName = "mysql_users_username"
+	mysqlUsersPassword = "mysql_users_password"
+	mysqlUsersHostname = "mysql_users_hostname"
+	mysqlUsersSchema   = "mysql_users_schemaname"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	username := os.Getenv(mysqlUsersUserName)
+	password := os.Getenv(mysqlUsersPassword)
+	host := os.Getenv(mysqlUsersHostname)
+	schema := os.Getenv(mysqlUsersSchema)
+
+	dataSourceName := fmt.Sprintf("%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, schema)
+	db, err := gorm.Open("mysql", dataSourceName)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// // Migrate the schema
+	db.AutoMigrate(&User{})
+}
